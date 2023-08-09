555d7e627e5660eb94fcfbfcb4f7a2fb635ff77f
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-08-09 07:27:51 -0300

    Adicionar arquivos de projeto.

---
+++ Conversor.cs
@@ -0,0 +1,12 @@
+﻿    namespace ExercicioDeFixacao_ConversorDeMoedas
+{
+    internal class Conversor
+    {
+        public static double valor;
+        public static double valorCompra;
+        public static double ValorPago ()
+        {
+            return (valor * valorCompra * 6 / 100) + (valor * valorCompra);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ExercicioDeFixacao_ConversorDeMoedas;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--- Conversor de Moeda (Dólar) ---");
+        
+        
+        Console.WriteLine();
+        Console.Write("Qual é a cotação do dólar? $");
+        Conversor.valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantos dólares você vai comprar? $");
+        Conversor.valorCompra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        Console.WriteLine("Valor a ser pago = R$" + Conversor.ValorPago().ToString("f2", CultureInfo.InvariantCulture));
+
+    }
+}
